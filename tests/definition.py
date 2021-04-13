cbe7a2d92bf47a7dce0451b8dac7eae92469ef28
micheleantonazzi <m7242872428>
2021-04-13 20:18:10 +0200

    fix tests on python 3.6

--- tests/definition.py
+++ tests/definition.py
@@ -1,15 +1,15 @@
+import sys
+
 import numpy as np
 
 def get_test_class():
-    def f_meta(self, a: int) -> int:
+    def f_meta(self, a: int, s: str = 'Hi') -> int:
         return a + 1
-
 
     class Meta(type):
         def __new__(cls, classname, supers, classdict):
             classdict['f_meta'] = f_meta
             return type.__new__(cls, classname, supers, classdict)
-
 
     class TestClass(metaclass=Meta):
         def __init__(self, a: np.ndarray):
@@ -25,16 +25,29 @@ def get_test_class():
 
     return TestClass
 
-
-stub_test_class = """class TestClass(object, metaclass=Meta):
-\tdef __init__(self, a: numpy.ndarray):
-\t\t...
-\tdef f(self, b: int) -> numpy.ndarray:
-\t\t\"\"\"
-\t\tciao
-\t\t:param b:
-\t\t:return:
-\t\t\"\"\"
-\t\t...
-\tdef f_meta(self, a: int) -> int:
-\t\t...\n"""
+if sys.version_info[0] == 3.6:
+    stub_test_class = """class TestClass(object, metaclass=Meta):
+    \tdef __init__(self, a:numpy.ndarray):
+    \t\t...
+    \tdef f(self, b:int) -> numpy.ndarray:
+    \t\t\"\"\"
+    \t\tciao
+    \t\t:param b:
+    \t\t:return:
+    \t\t\"\"\"
+    \t\t...
+    \tdef f_meta(self, a:int, s:str = 'Hi') -> int:
+    \t\t...\n"""
+else:
+    stub_test_class = """class TestClass(object, metaclass=Meta):
+    \tdef __init__(self, a: numpy.ndarray):
+    \t\t...
+    \tdef f(self, b: int) -> numpy.ndarray:
+    \t\t\"\"\"
+    \t\tciao
+    \t\t:param b:
+    \t\t:return:
+    \t\t\"\"\"
+    \t\t...
+    \tdef f_meta(self, a: int, s: str = 'Hi') -> int:
+    \t\t...\n"""
